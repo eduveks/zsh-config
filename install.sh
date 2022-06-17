90f2866aae44ec5f19825e7188f518798f3b0480
Eduardo Velasques <eduardo.velasques@sitana.pt>
2022-06-17 11:18:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@ chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-zsh
-
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
