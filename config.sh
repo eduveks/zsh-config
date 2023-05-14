b3f806791a965f76703f52fa84a723e6ad0ca61f
Eduardo Velasques <eduardo.velasques@sitana.pt>
2023-05-14 12:00:57 -0300

    Configuration file created to execute non-sudo commands separated from sudo commands of install.

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+awk '{sub("robbyrussell","ys")}1' ~/.zshrc > ~/.zshrc.tmp && mv ~/.zshrc.tmp ~/.zshrc
+
+awk '{sub("plugins=\\(git","plugins=(git emacs zsh-autosuggestions zsh-syntax-highlighting")}1' ~/.zshrc > ~/.zshrc.tmp && mv ~/.zshrc.tmp ~/.zshrc
+
+echo "alias e=\"bash -c emacs\"" >> ~/.zshrc
