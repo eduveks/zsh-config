0e06ec0de6e19f8b18394fb97b12ae105975c86e
Eduardo Velasques <eduardo.velasques@sitana.pt>
2023-05-14 12:02:23 -0300

    Install updated to use the configuration script with separated non-sudo commands.

--- install.sh
+++ install.sh
@@ -4,16 +4,4 @@ sudo apt update
 
 sudo apt install -y zsh git
 
-chsh -s $(which zsh)
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
-awk '{sub("robbyrussell","ys")}1' ~/.zshrc > ~/.zshrc.tmp && mv ~/.zshrc.tmp ~/.zshrc
-
-awk '{sub("plugins=\\(git","plugins=(git emacs zsh-autosuggestions zsh-syntax-highlighting")}1' ~/.zshrc > ~/.zshrc.tmp && mv ~/.zshrc.tmp ~/.zshrc
-
-echo "alias e=\"bash -c emacs\"" >> ~/.zshrc
+sh -c "$(curl -fsSL https://raw.github.com/eduveks/zsh-config/master/config.sh)"
